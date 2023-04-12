17a563a61ba2a21a24fd256c3832c2db7e28c406
Geronimo <pachecogero16@gmail.com>
2023-04-12 16:30:27 -0300

    no hice mucho, seguir desde 00:35:30

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from inicio import views
 
-app_name = "inicio"
+# app_name = "inicio"
 
 urlpatterns = [
-    path('', views.mi_vista),
-    path("mostrar-fecha/", views.mostrar_fecha),
-    path("prueba-template/", views.prueba_template),
-    path("crear-curso/", views.crear_curso),
-    path("prueba-render/", views.prueba_render),
+    path('', views.mi_vista, name ='inicio'),
+    path("mostrar-fecha/", views.mostrar_fecha, name ="mostrar_fecha"),
+    path("prueba-template/", views.prueba_template, name ="prueba_template"),
+    path("crear-curso/", views.crear_curso, name ="crear_curso"),
+    path("prueba-render/", views.prueba_render, name ="prueba_render"),
 ]

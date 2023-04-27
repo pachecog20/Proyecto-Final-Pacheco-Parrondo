70a07fffd64989c75a3c882b16e20c0fdfb60fac
Geronimo <pachecogero16@gmail.com>
2023-04-27 18:02:23 -0300

    me falta que solo el admin pueda crear clases y la app mensajeria, y retocar el sobre mi

--- inicio/urls.py
+++ inicio/urls.py
@@ -6,13 +6,14 @@ app_name = "inicio"
 urlpatterns = [
     path('', views.menu, name ='inicio'),
     
-    
+    path('sobre-mi/', views.sobre_mi, name='sobre_mi'),
     
    #path("Articulos/", views.articulos, name ="Articulos"),
    # path("Articulos/Lista/", views.lista_articulos, name ="listar_articulos"),
    # path("Articulos/<int:id_articulo>/Eliminar/", views.eliminar_articulo, name ="eliminar_articulos"),  
     #path("Articulos/<int:id_articulo>/Modificar/", views.modificar_articulo, name ="modificar_articulos"),  
-    path("Articulos/<int:pk>/", views.MostrarArticulo.as_view(), name ="mostrar_articulos"), 
+    path("Articulos/<int:pk>/", views.MostrarArticulo.as_view(), name ="mostrar_articulos"),
+     
     path("Articulos/Lista/", views.ListaArticulos.as_view(), name ="listar_articulos"),
     path("Articulos/", views.CrearArticulos.as_view(), name ="Articulos"),
     path("Articulos/<int:pk>/Modificar/", views.ModificarArticulos.as_view() , name ="modificar_articulos"), 
--- inicio/views.py
+++ inicio/views.py
@@ -14,62 +14,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 def menu(request):
     return render(request, "inicio/menu.html")
 
-
-
-def articulos(request):
-    if request.method == "POST":
-        formulario = CreacionArticuloFormulario(request.POST)
-        
-        if formulario.is_valid():
-            datos_correctos = formulario.cleaned_data
-        
-            curso = Articulo(nombre=datos_correctos['nombre'], color= datos_correctos['color'],version= datos_correctos['version'], marca= datos_correctos['marca'])
-            curso.save()
-            
-            return redirect("inicio:Articulos")
-    
-    formulario = CreacionArticuloFormulario()
-    return render(request, "inicio/articulos.html", {"formulario": formulario})    
-   
-
-
-def lista_articulos(request):
-    nombre_a_buscar= request.GET.get("nombre", None)
-    
-    if nombre_a_buscar:
-        articulos=Articulo.objects.filter(nombre__icontains=nombre_a_buscar)
-    else: 
-        articulos = Articulo.objects.all()
-    formulario_busqueda = BuscarArticulo()
-    return render(request, "inicio/lista_articulos.html", {"articulos":articulos, "formulario":formulario_busqueda} )     
-
-def eliminar_articulo(request, id_articulo):
-    articulo_a_eliminar = Articulo.objects.get(id=id_articulo)
-    articulo_a_eliminar.delete()
-    return redirect("inicio:listar_articulos")
-
-def mostrar_articulo(request, id_articulo):
-    articulo_a_mostrar = Articulo.objects.get(id=id_articulo)
-    return render(request, "inicio/mostrar_articulos.html", {"articulo_a_mostrar":articulo_a_mostrar} )  
-    
-
-def modificar_articulo(request, id_articulo):
-    articulo_a_modificar = Articulo.objects.get(id=id_articulo)
-    
-    if request.method == "POST":
-        formulario = ModificarArticuloFormulario(request.POST)
-        if formulario.is_valid():
-            data_limpia = formulario.cleaned_data
-            articulo_a_modificar.nombre = data_limpia["nombre"]
-            articulo_a_modificar.color = data_limpia["color"]
-            articulo_a_modificar.version = data_limpia["version"]
-            articulo_a_modificar.marca = data_limpia["marca"]
-            articulo_a_modificar.save()
-            return redirect("inicio:listar_articulos")
-    
-    formulario = ModificarArticuloFormulario(initial={"nombre":articulo_a_modificar.nombre,"color":articulo_a_modificar.color,"version":articulo_a_modificar.version,"marca":articulo_a_modificar.marca })
-    return render(request, "inicio/modificar_articulos.html", {"formulario": formulario, "id_articulo" : id_articulo})
-
+def sobre_mi(request):
+    return render(request, 'inicio/sobre_mi.html')
     
 
 class ListaArticulos(ListView):

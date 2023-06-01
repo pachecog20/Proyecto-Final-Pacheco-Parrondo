0a0019d412fb508356ca5f730fe4665dad7faaab
Geronimo <pachecogero16@gmail.com>
2023-06-01 15:35:49 -0300

    agregue campo ciudad a la edicion de usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class InformacionExtra(models.Model):
     avatar = models.ImageField(upload_to='avatares', null=True, blank=True)
+    city = models.CharField(max_length=20, null = True, blank = True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     
     
